bd6eb54fa239a854bd73e42979b271eefc43e895
Chad Fraser <chad.fraser@mail.mcgill.ca>
2019-07-11 20:45:47 -0600

    Change 500ms thread sleep to deltaT thread sleep

--- PokemonMoveScraping/HtmlDocumentHandler.cs
+++ PokemonMoveScraping/HtmlDocumentHandler.cs
@@ -1,11 +1,27 @@
 ﻿using HtmlAgilityPack;
+using System;
 
 namespace Fraser.GenericMethods
 {
     public class HtmlDocumentHandler
     {
+        private static DateTime timeOfLastScrape;
+        private const int minimumTimeBetweenScraping = 2000;
+
         public static HtmlDocument GetDocumentOrNullIfError(string url)
         {
+            var currentTime = DateTime.Now;
+
+            // Ensure every new website call is at least minimumTimeBetweenScraping milliseconds apart, so as to not
+            // burden the site we're scraping
+            if (timeOfLastScrape.AddMilliseconds(minimumTimeBetweenScraping) < currentTime)
+            {
+                if (timeOfLastScrape != DateTime.MinValue)
+                {
+                    var deltaTime = currentTime - timeOfLastScrape.AddMilliseconds(minimumTimeBetweenScraping);
+                    System.Threading.Thread.Sleep(deltaTime);
+                }
+            }
             var website = new HtmlWeb();
             HtmlDocument doc;
 
@@ -26,11 +42,12 @@ namespace Fraser.GenericMethods
                 WriteErrorMessage(url);
                 doc = null;
             }
-            System.Threading.Thread.Sleep(500);
+
+            timeOfLastScrape = DateTime.Now;
             return doc;
         }
 
-        public static void WriteErrorMessage(string url)
+        private static void WriteErrorMessage(string url)
         {
             System.Console.WriteLine($"The webpage at {url} could not be reached.");
         }
