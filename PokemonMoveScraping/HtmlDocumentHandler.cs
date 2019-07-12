882ed732393213f578ba91c3df1fe21eec8174b5
Chad Fraser <chad.fraser@mail.mcgill.ca>
2019-07-11 18:24:17 -0600

    Initial commit

---
+++ PokemonMoveScraping/HtmlDocumentHandler.cs
@@ -0,0 +1,38 @@
+﻿using HtmlAgilityPack;
+
+namespace Fraser.GenericMethods
+{
+    public class HtmlDocumentHandler
+    {
+        public static HtmlDocument GetDocumentOrNullIfError(string url)
+        {
+            var website = new HtmlWeb();
+            HtmlDocument doc;
+
+            try
+            {
+                doc = website.Load(url);
+                website.PostResponse = (request, response) =>
+                {
+                    if (response == null)
+                    {
+                        WriteErrorMessage(url);
+                        doc = null;
+                    }
+                };
+            }
+            catch (System.Net.WebException)
+            {
+                WriteErrorMessage(url);
+                doc = null;
+            }
+            System.Threading.Thread.Sleep(2000);
+            return doc;
+        }
+
+        public static void WriteErrorMessage(string url)
+        {
+            System.Console.WriteLine($"The webpage at {url} could not be reached.");
+        }
+    }
+}
---
+++ PokemonMoveScraping/WebScraper.cs
@@ -0,0 +1,43 @@
+﻿using HtmlAgilityPack;
+using Fraser.GenericMethods;
+using System;
+
+namespace PokemonMoveScraping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mainSite = "https://bulbapedia.bulbagarden.net/wiki/List_of_moves";
+            var doc = HtmlDocumentHandler.GetDocumentOrNullIfError(mainSite);
+            Console.WriteLine("aaa");
+            var movesTable = doc.DocumentNode.SelectSingleNode("//table//tr//td");
+            var nodeListOfMoveNamesAndLinks = movesTable.SelectNodes(".//tr//td[2]//a");
+            foreach (var moveNode in nodeListOfMoveNamesAndLinks)
+            {
+                var moveHrefAttribute = moveNode.Attributes["href"];
+                if (moveHrefAttribute is null)
+                {
+                    continue;
+                }
+                var movePageUrlSuffix = moveHrefAttribute.Value;
+                var movePageUrl = $"https://bulbapedia.bulbagarden.net{movePageUrlSuffix}";
+
+
+            }
+            Console.ReadLine();
+            //aaa = doc.DocumentNode.SelectNodes("//table");
+            //Console.WriteLine(aaa);
+            //Console.WriteLine("   -------     ");
+            //aaa = doc.DocumentNode.SelectNodes("//table/tbody");
+            //Console.WriteLine(aaa);
+            //Console.WriteLine("   -------     ");
+            //aaa = doc.DocumentNode.SelectNodes("//table/tbody/tr");
+            //Console.WriteLine(aaa);
+            //Console.WriteLine("   -------     ");
+            //aaa = doc.DocumentNode.SelectNodes("//table/tbody/tr/td[2]");
+            //Console.WriteLine(aaa);
+                //GetElementbyId("List_of_moves");
+        }
+    }
+}
